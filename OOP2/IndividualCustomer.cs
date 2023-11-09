c702c138f50e085dcd64d695c02f9c408b464b23
Mustafa Berat Demirci <34936699+mustafaberatdemirci@users.noreply.github.com>
2023-11-10 02:22:15 +0300

    Add files via upload

---
+++ OOP2/IndividualCustomer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    public class IndividualCustomer
+    {
+        public string IdentityNumber { get; set; } 
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
