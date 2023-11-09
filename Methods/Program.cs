b901f69ecd7cfcf81e698aa689e29db48a15c389
Mustafa Berat Demirci <34936699+mustafaberatdemirci@users.noreply.github.com>
2023-11-10 02:20:38 +0300

    Method

---
+++ Methods/Program.cs
@@ -0,0 +1,42 @@
+﻿using Methods;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Product product1 = new Product(); 
+        product1.Id = 1;
+        product1.Name = "Apple";
+        product1.Price = 15;
+        product1.Description = "Amasya Apple";
+        product1.UnitStock = 10;
+        Product product2 = new Product();
+        product2.Id = 2;
+        product2.Name = "Watermelon";
+        product2.Price = 80;
+        product2.Description = "Diyarbakır Watermelon";
+        product2.UnitStock = 20;
+
+        Product[] products = new Product[] { product1, product2 };
+        
+        foreach (Product product in products)
+        {
+            Console.WriteLine("Id: {0} \nName: {1} \nPrice: {2} " +
+                "\nDescription: {3}\n ", product.Id, product.Name, product.Price, product.Description);
+            Console.WriteLine("-------------------------------");
+        }
+
+        Console.WriteLine("---------------METHODS----------------");
+               
+        ShoppingCartManager shoppingCartManager = new ShoppingCartManager();
+        shoppingCartManager.Add(product1); //Add Method
+        shoppingCartManager.Add(product2);
+
+        Console.WriteLine("------------------------------------------");
+
+        shoppingCartManager.Add2(1, "Pear", 12, "Green Pear", 10); //Add2 Method
+        shoppingCartManager.Add2(2, "Apple", 15, "Amasya Apple", 10);
+        shoppingCartManager.Add2(3, "Watermelon", 80, "Diyarbakır Watermelon", 20);
+
+    }
+}
---
+++ Methods/ShoppingCartManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class ShoppingCartManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Congrats. Shopping Cart Added : " + product.Name);
+        }
+
+        public void Add2(int Id, string Name, double Price, string Description, int UnitStock)
+        {
+            Console.WriteLine("Congrats. Shopping Cart Added2 : " + Name);
+        }
+    }
+}
