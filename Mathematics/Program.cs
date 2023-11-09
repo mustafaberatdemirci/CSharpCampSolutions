821cf23bebb4c9b116d33e8a4ea7efbea40f92d6
Mustafa Berat Demirci <34936699+mustafaberatdemirci@users.noreply.github.com>
2023-11-10 02:19:42 +0300

    Add files via upload

---
+++ Mathematics/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Mathematics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FourOperations fourOperations = new FourOperations();
+            fourOperations.Sum(5, 6);
+            fourOperations.Sum(6, 9);
+
+        }
+    }
+}
