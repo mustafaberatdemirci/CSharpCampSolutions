df184360d52c166b5799d87a776998b5b483c2b1
Mustafa Berat Demirci <34936699+mustafaberatdemirci@users.noreply.github.com>
2023-11-10 02:18:32 +0300

    Generics

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("Engin");
+
+            List<string> list = new List<string>();
+            Console.WriteLine(list.Count);
+            //Console.WriteLine(names.Add);
+
+        }
+    }
+}
